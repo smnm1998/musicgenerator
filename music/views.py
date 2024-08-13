2f32363b859cf36e04a0d314ec9be636521b2e6d
AntVerrrrr <jhs789654123@gmail.com>
2024-08-13 16:51:46 +0900

    chore: cloudbuild.yaml gcloud가져오기

--- music/views.py
+++ music/views.py
@@ -1,7 +1,36 @@
+# from django.shortcuts import render
+# from google.cloud import storage
+# from django.conf import settings
+# from .forms import ImageUploadForm
+#
+# def upload_image(request):
+#     if request.method == 'POST':
+#         form = ImageUploadForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             image = form.cleaned_data['image']
+#
+#             # Google Cloud Storage 클라이언트 설정
+#             client = storage.Client(credentials=settings.GS_CREDENTIALS)
+#             bucket = client.bucket(settings.GS_BUCKET_NAME)
+#
+#             # 이미지 업로드
+#             blob = bucket.blob(image.name)
+#             blob.upload_from_file(image, content_type=image.content_type)
+#
+#             # 업로드된 파일의 URL 얻기
+#             uploaded_file_url = blob.public_url
+#
+#             return render(request, 'upload.html', {'form': form, 'uploaded_file_url': uploaded_file_url})
+#     else:
+#         form = ImageUploadForm()
+#     return render(request, 'upload.html', {'form': form})
+
+
 from django.shortcuts import render
 from google.cloud import storage
 from django.conf import settings
 from .forms import ImageUploadForm
+import os
 
 def upload_image(request):
     if request.method == 'POST':
@@ -13,8 +42,9 @@ def upload_image(request):
             client = storage.Client(credentials=settings.GS_CREDENTIALS)
             bucket = client.bucket(settings.GS_BUCKET_NAME)
 
-            # 이미지 업로드
-            blob = bucket.blob(image.name)
+            # 이미지 파일을 'uploads/image/' 디렉터리에 저장
+            file_path = os.path.join('uploads/image/', image.name)
+            blob = bucket.blob(file_path)
             blob.upload_from_file(image, content_type=image.content_type)
 
             # 업로드된 파일의 URL 얻기
@@ -24,3 +54,6 @@ def upload_image(request):
     else:
         form = ImageUploadForm()
     return render(request, 'upload.html', {'form': form})
+
+
+
