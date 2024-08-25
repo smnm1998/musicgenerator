d694d19aa6bb1289b715a986510e2255e58c2dbe
smnm1998 <dltpals7498@gmail.com>
2024-08-25 18:03:48 +0900

    feat: css 수정 및 프론트엔드 수정

--- music/static/music/js/result_page.js
+++ music/static/music/js/result_page.js
@@ -9,6 +9,12 @@ document.addEventListener("DOMContentLoaded", function() {
     const progBarInner = document.querySelector('.prog-bar-inner');
     const albumArtImage = document.getElementById('albumart-image');
 
+    // 페이지 로드 시 자동 재생 상태 확인 및 설정
+    if (!audioPlayer.paused) {
+        playPauseButton.textContent = '❚❚'; // 자동 재생 시 일시정지 버튼 표시
+        albumArtImage.style.animationPlayState = 'running'; // CD 회전 애니메이션 시작
+    }
+
     // 음악 재생 시간 업데이트
     audioPlayer.addEventListener('timeupdate', function() {
         const currentTime = audioPlayer.currentTime;
@@ -58,6 +64,21 @@ document.addEventListener("DOMContentLoaded", function() {
         const clickPositionPercent = offsetX / width; // 클릭한 위치를 백분율로 계산
         const duration = audioPlayer.duration;
         audioPlayer.currentTime = duration * clickPositionPercent; // 재생 시간을 클릭된 위치에 맞춰 설정
+    });
+
+    // 음원이 끝날 때 실행
+    audioPlayer.addEventListener('ended', function() {
+        console.log("음원이 끝났습니다."); // 이 로그가 출력되는지 확인
+        // 진행 바를 0으로 초기화
+        progBarInner.style.width = '0%';
+        currentTimeElement.textContent = '0:00';
+
+        // CD 애니메이션 멈춤 및 재생 버튼 초기화
+        playPauseButton.textContent = '▶'; // 다시 누르면 재생 아이콘으로 변경
+        albumArtImage.style.animationPlayState = 'paused'; // CD 회전 애니메이션 일시정지
+
+        // 재생 위치를 처음으로 되돌림
+        audioPlayer.currentTime = 0;
     });
 
     // 시간을 mm:ss 형식으로 변환하는 함수
